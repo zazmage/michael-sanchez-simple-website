3692616b7a9b09482e013ef014b417181c898c61
Daniel Castano <daniel.zapata.682@gmail.com>
2022-10-03 07:20:17 +1000

    Fixed paste facebook

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -34,6 +34,47 @@ interface State extends SnackbarOrigin {
   open: boolean;
 }
 
+function copyTextToClipboard(text: string) {
+  let textArea = document.createElement("textarea");
+
+  // Place in the top-left corner of screen regardless of scroll position.
+  textArea.style.position = "fixed";
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = "2em";
+  textArea.style.height = "2em";
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = "0";
+
+  // Clean up any borders.
+  textArea.style.border = "none";
+  textArea.style.outline = "none";
+  textArea.style.boxShadow = "none";
+
+  // Avoid flash of the white box if rendered for any reason.
+  textArea.style.background = "transparent";
+
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    let successful = document.execCommand("copy");
+    let msg = successful ? "successful" : "unsuccessful";
+    console.log("Copying text command was " + msg);
+  } catch (err) {
+    console.log("Oops, unable to copy");
+  }
+
+  document.body.removeChild(textArea);
+}
+
 const TextEditor = () => {
   const [value, setValue] = useState<string>("");
   const [plainText, setPlainText] = useState<string>("");
@@ -85,10 +126,8 @@ const TextEditor = () => {
         .split("\n")
         .map((el) => el.trim())
         .join("\n")
-        .replaceAll("\n", "<br>");
-      const blobInput = new Blob([formattedValue], { type: "text/html" });
-      const clipboardItemInput = new ClipboardItem({ "text/html": blobInput });
-      navigator.clipboard.write([clipboardItemInput]);
+        .replaceAll(/<[^>]*>?/gm, "");
+      copyTextToClipboard(formattedValue);
     }
     setState({
       open: true,
